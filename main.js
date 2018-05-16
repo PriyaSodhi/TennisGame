651b3fba0d91f31ea76699b487b85dc53e029e06
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-05-17 11:41:11 +1200

    Add Movement and time functionality in game
    Create draw function to group all draw code
    Use variable to vary shape draw Position
    Show motion over time, dynamic animation

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
- function game() {
-   var canvas;
-   var canvasContext;
-   console.log("Arcade Game");
+var canvas;
+var canvasContext;
+var ballX = 50;
+
+ function game(){
    canvas = document.getElementById('gameCanvas');
    canvasContext = canvas.getContext('2d');
+   setInterval(draw, 50) // draw function is will take a pause of 1000ms.
+ }
+ function draw() {
+   ballX = ballX + 5;
+   console.log("Arcade Game");
+   console.log(ballX);
    canvasContext.fillStyle = 'black';
    canvasContext.fillRect(0, 0, canvas.width, canvas.height);
-   canvasContext.fillStyle = 'red';
-   canvasContext.fillRect(100,200,50,25);
    canvasContext.fillStyle = 'white';
-   canvasContext.fillRect(300,200,200,200);
+   canvasContext.fillRect(225,210,200,200);
+   canvasContext.fillStyle = 'red';
+   canvasContext.fillRect(ballX,100,10,10);
 }
