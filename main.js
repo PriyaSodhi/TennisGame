1afee9f44ec21d91f4545b36c26ac49d108854b0
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-05-17 15:25:18 +1200

    Bouncing the ball
    Create a variable to make speed changes
    Get the ball moving in the opposite direction
    Bounce the ball off the right canvas side
    Keep the ball speed same after wall bounce
    Bounce the ball off the left canvas side
    Create functions for drawing the filled shapes
    Use comments

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var canvas;
 var canvasContext;
 var ballX = 50;
+var ballSpeedX = 10;
 
  function game(){
    var framerPerSecond = 30
@@ -12,21 +13,29 @@ var ballX = 50;
    }, 1000/framerPerSecond); // draw function is will take a pause of 1000ms.
  }
 
- // function callMoveDraw(){
- //   move();
- //   draw();
- // }
-
  function  move(){
-   ballX = ballX + 5;
+   ballX = ballX + ballSpeedX;
+   if(ballX >  canvas.width) {
+     ballSpeedX = -ballSpeedX;
+   }
+   if (ballX < 0) {
+     ballSpeedX = -ballSpeedX;
+   }
  }
- function draw() { 
+
+ function draw() {
    console.log("Arcade Game");
    console.log(ballX);
-   canvasContext.fillStyle = 'black';
-   canvasContext.fillRect(0, 0, canvas.width, canvas.height);
-   canvasContext.fillStyle = 'white';
-   canvasContext.fillRect(10,210,10,100);
-   canvasContext.fillStyle = 'red';
-   canvasContext.fillRect(ballX,100,10,10);
+   //next line blanks out the screen black
+   colorRect(0, 0, canvas.width, canvas.height, 'black');
+   //this is left player paddle
+   colorRect(10, 210, 10, 100, 'white');
+   // next line draws the ball
+   colorRect(ballX, 100, 10, 10, 'red');
+}
+
+function colorRect(leftX, topY, width, height, drawColor)
+{
+  canvasContext.fillStyle = drawColor;
+  canvasContext.fillRect(leftX, topY, width, height, drawColor);
 }
