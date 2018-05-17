b3bc77911d8b8b4d820333cae97007ef0bd84826
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-05-17 14:41:34 +1200

    Clean up the code
    Set Update frequency as frames per second
    Move and resize the box to make it a paddle
    Seperate the move code from the draw code
    Writing a function using inline form

--- main.js
+++ main.js
@@ -3,18 +3,30 @@ var canvasContext;
 var ballX = 50;
 
  function game(){
+   var framerPerSecond = 30
    canvas = document.getElementById('gameCanvas');
    canvasContext = canvas.getContext('2d');
-   setInterval(draw, 50) // draw function is will take a pause of 1000ms.
+   setInterval(function() { //inline function
+     move();
+     draw();
+   }, 1000/framerPerSecond); // draw function is will take a pause of 1000ms.
  }
- function draw() {
+
+ // function callMoveDraw(){
+ //   move();
+ //   draw();
+ // }
+
+ function  move(){
    ballX = ballX + 5;
+ }
+ function draw() { 
    console.log("Arcade Game");
    console.log(ballX);
    canvasContext.fillStyle = 'black';
    canvasContext.fillRect(0, 0, canvas.width, canvas.height);
    canvasContext.fillStyle = 'white';
-   canvasContext.fillRect(225,210,200,200);
+   canvasContext.fillRect(10,210,10,100);
    canvasContext.fillStyle = 'red';
    canvasContext.fillRect(ballX,100,10,10);
 }
