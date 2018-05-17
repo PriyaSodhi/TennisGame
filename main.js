0f009ee6c484a53b8e7d360cc6cb9a04b28db69f
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-05-18 10:35:24 +1200

    Ball 2D motion and Paddle
    Move and bounce the ball vertically
    Define labelled values for paddle position and size
    Find the mouse position relative to game canvas
    Set Paddle’s position value whenever mouse moves
    Align the paddle’s centre on the mouse’s Y position

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 var canvas;
 var canvasContext;
 var ballX = 50;
+var ballY = 50;
 var ballSpeedX = 10;
+var ballSpeedY = 10;
+
+var paddle1Y =  250;
+const PADDLE_HEIGHT = 100;
 
  function game(){
    var framerPerSecond = 30
@@ -11,15 +16,33 @@ var ballSpeedX = 10;
      move();
      draw();
    }, 1000/framerPerSecond); // draw function is will take a pause of 1000ms.
+   /*
+   next line will calculate the mouse coordinates and will
+   change the y coordinate of paddle correspondingly
+   */
+   canvas.addEventListener("mousemove", function(event) {
+     var mousePos = calculateMousePosition(event);
+     //Align the paddle’s centre on the mouse’s Y position
+     paddle1Y = mousePos.y - (PADDLE_HEIGHT/2);
+
+   });
  }
 
  function  move(){
    ballX = ballX + ballSpeedX;
+   ballY = ballY + ballSpeedY;
+
    if(ballX >  canvas.width) {
      ballSpeedX = -ballSpeedX;
    }
    if (ballX < 0) {
      ballSpeedX = -ballSpeedX;
+   }
+   if(ballY > canvas.height) {
+     ballSpeedY = -ballSpeedY;
+   }
+   if(ballY < 0) {
+     ballSpeedY = -ballSpeedY;
    }
  }
 
@@ -29,13 +52,13 @@ var ballSpeedX = 10;
    //next line blanks out the screen black
    colorRect(0, 0, canvas.width, canvas.height, 'black');
    //this is left player paddle
-   colorRect(10, 210, 10, 100, 'white');
+   colorRect(10, paddle1Y, 10, PADDLE_HEIGHT , 'white');
     // next line draws the ball
-  colorCircle(ballX, 150, 10, 'white');
+   colorCircle(ballX, ballY, 10, 'white');
 }
 
 function colorCircle(centerX, centerY, radius, drawColor ) {
-  canvasContext.fillStyle = drawColor; 
+  canvasContext.fillStyle = drawColor;
   canvasContext.beginPath();
   canvasContext.arc(centerX, centerY, radius , 0, Math.PI * 2, true);
   canvasContext.fill();
@@ -44,4 +67,16 @@ function colorCircle(centerX, centerY, radius, drawColor ) {
 function colorRect(leftX, topY, width, height, drawColor) {
   canvasContext.fillStyle = drawColor;
   canvasContext.fillRect(leftX, topY, width, height, drawColor);
+}
+
+function calculateMousePosition(event) {
+  //returns coordinates relative to the viewport i.e canvas
+  var rect = canvas.getBoundingClientRect();
+  var root = document.documentElement;
+  var mouseX = event.clientX - rect.left - root.scrollLeft;
+  var mouseY = event.clientY - rect.top - root.scrollTop;
+  return {
+    x:mouseX,
+    y:mouseY
+  };
 }
